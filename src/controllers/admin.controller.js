596de4700b4fdaa1fa85bf812c50489f37ce75ce
zs153 <tmp153@gmail.com>
2023-04-13 14:37:29 +0200

    upd

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,48 +1,273 @@
 import axios from 'axios'
 import { puertoAPI, serverAPI } from '../config/settings'
+import { tiposMovimiento } from '../public/js/enumeraciones'
 
-export const mainPage = async (req, res) => {
+export const cartasPage = async (req, res) => {
   const user = req.user
 
+  const dir = req.query.dir ? req.query.dir : 'next'
+  const limit = req.query.limit ? req.query.limit : 10
+  const part = req.query.part ? req.query.part.toUpperCase() : ''
+
+  let hasPrevCartas = false
+  let cursor = req.query.cursor ? JSON.parse(req.query.cursor) : null
+  let context = {}
+
+  if (cursor) {
+    hasPrevCartas = true
+    context = {
+      limit: limit + 1,
+      direction: dir,
+      cursor: JSON.parse(convertCursorToNode(JSON.stringify(cursor))),
+      part,
+    }
+  } else {
+    context = {
+      limit: limit + 1,
+      direction: dir,
+      cursor: {
+        next: 0,
+        prev: 0,
+      },
+      part,
+    }
+  }
+
   try {
-    const perfiles = await axios.post(`http://${serverAPI}:${puertoAPI}/api/estados/usuarios/perfiles`, {
-      estado,
-    })
-    const matriculas = await axios.post(`http://${serverAPI}:${puertoAPI}/api/formacion/matriculas`, {
+    const result = await axios.post(`http://${serverAPI}:${puertoAPI}/api/cartas`, {
       context,
     })
 
-    let userid = ''
-    let data = []
-    perfiles.data.data.map(itm => {
-      if (itm.TIPEST === 2) {
-        const hora = new Date().toTimeString().slice(0, 5)
-        if (!(hora >= itm.DESHOR && hora <= itm.HASHOR)) {
-          itm.TIPEST = 1
+    let cartas = result.data.data
+    let hasNextCartas = cartas.length === limit + 1
+    let nextCursor = 0
+    let prevCursor = 0
+
+    if (hasNextCartas) {
+      const nextCursorCarta = dir === 'next' ? cartas[limit - 1] : cartas[0]
+      const prevCursorCarta = dir === 'next' ? cartas[0] : cartas[limit - 1]
+      nextCursor = nextCursorCarta.IDCART
+      prevCursor = prevCursorCarta.IDCART
+
+      cartas.pop()
+    } else {
+      if (dir === 'prev') {
+        context = {
+          limit: limit + 1,
+          direction: 'next',
+          cursor: {
+            next: 0,
+            prev: 0
+          },
+          part,
         }
 
-        if (itm.USERID === userid) {
-          if (itm.TIPEST === 1) {
-            itm = data.slice(-1)[0]
-          }
-          data.splice(-1)
+        const result = await axios.post(`http://${serverAPI}:${puertoAPI}/api/cartas`, {
+          context,
+        })
+
+        cartas = result.data.data
+        hasNextCartas = cartas.length === limit + 1
+
+        if (hasNextCartas) {
+          const nextCursorCarta = cartas[limit - 1]
+          const prevCursorCarta = cartas[0]
+          nextCursor = nextCursorCarta.IDCART
+          prevCursor = prevCursorCarta.IDCART
+
+          cartas.pop()
         }
-        userid = itm.USERID
+
+        hasPrevCartas = false
+      } else {
+        if (cursor) {
+          const prevCursorCarta = cartas[0]
+          prevCursor = prevCursorCarta.IDCART
+          hasPrevCartas = true
+        } else {
+          hasPrevCartas = false
+        }
+
+        hasNextCartas = false
       }
-      data.push(itm)
-    })
-
-    const datos = {
-      estados: data,
-      hayMatricula: matriculas.data.data.length > 0 ? true : false,
     }
 
-    res.render('admin', { user, datos })
-  } catch (error) {
-    const msg = 'No se ha podido acceder a los datos de la aplicación.'
+    cursor = {
+      next: nextCursor,
+      prev: prevCursor,
+    }
+    const datos = {
+      user,
+      limit,
+      cartas,
+      hasPrevCartas,
+      hasNextCartas,
+      cursor: convertNodeToCursor(JSON.stringify(cursor)),
+    }
 
-    res.render('user/error400', {
-      alerts: [{ msg }],
-    })
+
+    res.render('admin/cartas', { user, datos })
+  } catch (error) {
+    console.log('error....',error);
+    if (error.response?.status === 400) {
+      res.render("admin/error400", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    } else {
+      res.render("admin/error500", {
+        alerts: [{ msg: error }],
+      });
+    }
   }
+}
+export const addPage = async (req, res) => {
+  const user = req.user
+
+  try {
+    const datos = {
+    }
+
+    res.render('admin/cartas/add', { user, datos })
+  } catch (error) {
+    if (error.response.status === 400) {
+      res.render("admin/error400", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    } else {
+      res.render("admin/error500", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    }
+  }
+}
+export const editPage = async (req, res) => {
+  const user = req.user
+  const context = {
+    IDCART: req.params.id,
+  }
+
+  try {
+    const carta = await axios.post(`http://${serverAPI}:${puertoAPI}/api/carta`, {
+      context,
+    })
+    const datos = {
+      carta: carta.data.data,
+    }
+
+    res.render('admin/cartas/edit', { user, datos })
+  } catch (error) {
+    if (error.response.status === 400) {
+      res.render("admin/error400", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    } else {
+      res.render("admin/error500", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    }
+  }
+}
+
+// proc
+export const insert = async (req, res) => {
+  const user = req.user
+
+  try {
+      const carta = {
+        NOMCAR: req.body.nomcar.toUpperCase(),
+        CONCAR: req.body.concar,
+        FICCAR: req.body.ficcar,
+      }
+      const movimiento = {
+        USUMOV: user.id,
+        TIPMOV: tiposMovimiento.crearCarta,
+      }
+
+      await axios.post(`http://${serverAPI}:${puertoAPI}/api/cartas/insert`, {
+        carta,
+        movimiento,
+      })
+
+      res.redirect('/admin/cartas')
+  } catch (error) {
+    if (error.response.status === 400) {
+      res.render("admin/error400", {
+        alerts: [{ msg: error.response.data.data }],
+      });
+    } else {
+      res.render("admin/error500", {
+        alerts: [{ msg: error.response.data.data }],
+      });
+    }
+  }
+}
+export const update = async (req, res) => {
+  const user = req.user
+  const carta = {
+    IDCART: req.body.idcart,
+    NOMCAR: req.body.nomcar.toUpperCase(),
+    FICCAR: req.body.ficcar,
+    CONCAR: req.body.concar,
+  }
+  const movimiento = {
+    USUMOV: user.id,
+    TIPMOV: tiposMovimiento.modificarCarta,
+  }
+
+  console.log(carta,movimiento);
+  try {
+    await axios.post(`http://${serverAPI}:${puertoAPI}/api/cartas/update`, {
+      carta,
+      movimiento,
+    })
+
+    res.redirect('/admin/cartas')
+  } catch (error) {
+    if (error.response?.status === 400) {
+      res.render("admin/error400", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    } else {
+      res.render("admin/error500", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    }
+  }
+}
+export const remove = async (req, res) => {
+  const user = req.user
+  const carta = {
+    IDCART: req.body.idcart,
+  }
+  const movimiento = {
+    USUMOV: user.id,
+    TIPMOV: tiposMovimiento.borrarCarta,
+  }
+
+  try {
+    await axios.post(`http://${serverAPI}:${puertoAPI}/api/cartas/delete`, {
+      carta,
+      movimiento,
+    })
+
+    res.redirect('/admin/cartas')
+  } catch (error) {
+    if (error.response?.status === 400) {
+      res.render("admin/error400", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    } else {
+      res.render("admin/error500", {
+        alerts: [{ msg: error.response.data.msg }],
+      });
+    }
+  }
+}
+
+// helpers
+const convertNodeToCursor = (node) => {
+  return new Buffer.from(node, 'binary').toString('base64')
+}
+const convertCursorToNode = (cursor) => {
+  return new Buffer.from(cursor, 'base64').toString('binary')
 }
--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,12 +1,24 @@
 import express from 'express'
 import authRoutes from '../middleware/auth'
 import {
-  mainPage,
+  addPage,
+  cartasPage,
+  editPage,
+  insert,
+  remove,
+  update,
 } from '../controllers/admin.controller'
 
 const adminRouter = express.Router()
 
 // paginas
-adminRouter.get('/', authRoutes, mainPage)
+adminRouter.get('/cartas', authRoutes, cartasPage)
+adminRouter.get('/cartas/add', authRoutes, addPage)
+adminRouter.get('/cartas/edit/:id', authRoutes, editPage)
+
+// procedures
+adminRouter.post('/cartas/insert', authRoutes, insert)
+adminRouter.post('/cartas/update', authRoutes, update)
+adminRouter.post('/cartas/delete', authRoutes, remove)
 
 export default adminRouter
